b09e7a88814686c0e57b372fb17bdc3d2fa6a428
curranzach <curran.zach@gmail.com>
2021-10-20 15:40:21 -0400

    Added Google Maps API

--- red-client/src/pages/home.js
+++ red-client/src/pages/home.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const mapStyles = {
+    width: '1000px',
+    height: '1000px',
+  };
 
 class home extends Component {
     render(){
         return(
-            <center>Renewable Energy Dashboard</center>
+            <div>
+            <p>Renewable Energy Dashboard</p>
+            <Map
+                google={this.props.google}
+                zoom={8}
+                style={mapStyles}
+                initialCenter={{ lat: 47.444, lng: -122.176}}
+            />
+          </div>
         )
     }
 }
 
-export default home
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyAsyLGrmG68jX6To64YSvfEkkyJ7oGuuuw'
+  })(home);
